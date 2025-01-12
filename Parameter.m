8b2ebdf1961de8dba387112e8be7feafed8aa5cd
B4LM <158221249+B4LM@users.noreply.github.com>
2025-01-12 18:46:11 +0100

    12012025

---
+++ Parameter.m
@@ -0,0 +1,29 @@
+clc; clear; close all; 
+
+%% general
+
+g = 9.81;                    % [m/s^2]
+m_ges = 0.53;                % [kg]
+m_last = 0.51;               % [kg]
+Achsabstand = 0.15;          % [m]
+
+
+
+%% Motormodell
+
+motorinertia = 10;           % [kg*m^2]
+motorinductivity = 0.0005;   % [H]
+motorresistance =  3;        % [Ohm]
+motordamping = 0.00174;      % [?]
+motortorquekoef = 0.00174;   % [N*m / A]
+motorBackEMFkoef = 0.00174;  % [?]
+
+%% Reifenmodell
+
+m_Reifen = 0.01;             % [kg]
+mue = 0.002;                 % [1]
+Reifen_Radius = 0.05;        % [m]
+
+
+
+
