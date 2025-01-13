056d7d9acd5e0e0bbc2db7e1d995aff77ea8ff1d
B4LM <158221249+B4LM@users.noreply.github.com>
2025-01-13 17:57:48 +0100

    13012025 Subsysteme müssen noch neu eingefügt werden

--- Parameter.m
+++ Parameter.m
@@ -6,15 +6,15 @@ g = 9.81;                    % [m/s^2]
 m_ges = 0.53;                % [kg]
 m_last = 0.51;               % [kg]
 Achsabstand = 0.15;          % [m]
-
+Motoruebersetzung = 1/50;    % [1]
 
 
 %% Motormodell
 
-motorinertia = 10;           % [kg*m^2]
+motorinertia = 0.847 * 10^-6;% [kg*m^2]
 motorinductivity = 0.0005;   % [H]
 motorresistance =  3;        % [Ohm]
-motordamping = 0.00174;      % [?]
+motordamping = 0.015;        % [?]
 motortorquekoef = 0.00174;   % [N*m / A]
 motorBackEMFkoef = 0.00174;  % [?]
 
