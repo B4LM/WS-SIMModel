1218b71dc746d4956f2d43486761453c136fcfa4
B4LM <158221249+B4LM@users.noreply.github.com>
2025-01-14 20:13:30 +0100

    14012025 SimpleBotV2

--- Parameter.m
+++ Parameter.m
@@ -24,6 +24,29 @@ m_Reifen = 0.01;             % [kg]
 mue = 0.002;                 % [1]
 Reifen_Radius = 0.05;        % [m]
 
+%% motor state space model
+
+A = [0 1 0; 0 -motordamping/motorinertia motortorquekoef/motorinertia;0 -motorBackEMFkoef/motorinductivity -motorresistance/motorinductivity];
+B = [0; 0; 1/motorinductivity];
+CT = [0 1 0];
+D = [0];
+
+x0 = [0; 0.01; 0];
+
+%% second try state space model
+
+A22 = -motordamping/(motorinertia + m_Reifen * Reifen_Radius^2);
+A23 = motortorquekoef/(motorinertia + m_Reifen * Reifen_Radius^2);
+A32 = -motorBackEMFkoef/motorinductivity;
+A33 = -motorresistance/motorinductivity;
+
+A_neu = [0 1 0; 0 A22 A23; 0 A32 A33];
+B_neu = [0; 0; 1/motorinductivity];
+E_neu = [0; -((m_last + m_Reifen) * g * mue * Reifen_Radius); 0 ];
+CT_nvel = [0 1 0];
+CT_npos = [1 0 0];
+x0_neu = [0; 0.01; 0];
+
 
 
 
