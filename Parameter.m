62ff12f0358ab166da458437653bbf0ef4d208e1
B4LM <158221249+B4LM@users.noreply.github.com>
2025-01-15 20:10:50 +0100

    15012025

--- Parameter.m
+++ Parameter.m
@@ -14,9 +14,9 @@ Motoruebersetzung = 1/50;    % [1]
 motorinertia = 0.847 * 10^-6;% [kg*m^2]
 motorinductivity = 0.0005;   % [H]
 motorresistance =  3;        % [Ohm]
-motordamping = 0.015;        % [?]
-motortorquekoef = 0.00174;   % [N*m / A]
-motorBackEMFkoef = 0.00174;  % [?]
+motordamping = 0.005;        % [?]
+motortorquekoef = 0.06533;   % [N*m / A] 0.00174
+motorBackEMFkoef = 0.06533;  % [?] 0.00174
 
 %% Reifenmodell
 
@@ -35,17 +35,30 @@ x0 = [0; 0.01; 0];
 
 %% second try state space model
 
-A22 = -motordamping/(motorinertia + m_Reifen * Reifen_Radius^2);
-A23 = motortorquekoef/(motorinertia + m_Reifen * Reifen_Radius^2);
+A22 = -motordamping/(motorinertia + (m_Reifen * Reifen_Radius^2)/50);
+A23 = motortorquekoef/(motorinertia + (m_Reifen * Reifen_Radius^2)/50);
 A32 = -motorBackEMFkoef/motorinductivity;
 A33 = -motorresistance/motorinductivity;
 
 A_neu = [0 1 0; 0 A22 A23; 0 A32 A33];
 B_neu = [0; 0; 1/motorinductivity];
-E_neu = [0; -((m_last + m_Reifen) * g * mue * Reifen_Radius); 0 ];
+E_neu = [0; -((m_last/2 + m_Reifen) * g * mue * Reifen_Radius); 0 ];
 CT_nvel = [0 1 0];
 CT_npos = [1 0 0];
 x0_neu = [0; 0.01; 0];
+
+%% third try state space model
+A22n = -motordamping/motorinertia;
+A23n = motortorquekoef/motorinertia;
+A_neu2 = [0 1 0; 0 A22n A23n; 0 A32 A33];
+E_neu2 = [0; 0; 0 ];
+
+%% testing
+
+%TestV = 12;
+
+%omega = (1/(motordamping +((motortorquekoef^2)/motorresistance))) * ((motortorquekoef/motorresistance) * TestV - (m_last/2 + m_Reifen) * g *mue * Reifen_Radius)
+
 
 
 
