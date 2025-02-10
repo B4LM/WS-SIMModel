b4ddf68137be2163746049a9a24cb2abceefad76
B4LM <158221249+B4LM@users.noreply.github.com>
2025-02-10 20:53:41 +0100

    12022025abend

--- Kinematics_test.m
+++ Kinematics_test.m
@@ -1,6 +1,6 @@
 omega1 = 2 ;
-omega2 = 1;
-omegadot1 = 1;
+omega2 = 7;
+omegadot1 = 3;
 omegadot2 = 1;
 phi = 30;
 rs = [0;0];
@@ -12,16 +12,25 @@ r20 = rs + RotPhi * [B_dis/3 ; -Achsabstand/2];
 
 v10 = RotPhi * [1;0] * omega1 * Reifen_Radius;
 v20 = RotPhi * [1;0] * omega2 * Reifen_Radius;
+vs = RotPhi * [1;0] * ((omega1 * Reifen_Radius + omega2 * Reifen_Radius)/2);
 
-Avel = [1 0 r10(1) ; 0 1 r10(2); 1 0 r20(1); 0 1 r20(2)];
-Vel_Result = linsolve(Avel,[v10;v20])
+o_numerator = (v20(1) - v10(1)) * (r10(2) - r20(2)) + (v20(2) - v10(2)) * (r20(1) - r10(1));
+o_denominator = (r10(2) - r20(2))^2 + (r20(1) - r10(1))^2;
+omega_Bot = o_numerator / o_denominator;
 
-%a1 = phivec * omegadot1 * Reifen_Radius;
-%a2 = phivec * omegadot2 * Reifen_Radius;
-r_neu = sqrt(B_dis^2 +(Achsabstand/2)^2);
-v1_neu = omega1 * Reifen_Radius;
-v2_neu = omega2 * Reifen_Radius;
-Vel_Result_neu = linsolve([1 r_neu; 1 r_neu], [v1_neu; v2_neu])
+a10 = RotPhi * [1;0] * omegadot1 * Reifen_Radius;
+a20 = RotPhi * [1;0] * omegadot2 * Reifen_Radius;
+as = RotPhi * [1;0] * ((omegadot1 * Reifen_Radius + omegadot2 * Reifen_Radius)/2);
+
+odot_numerator = (a20(1) - a10(1) - omega_Bot^2 * (r10(1) - r20(1))) * (r10(2) - r20(2)) + (a20(2) - a10(2) - omega_Bot^2 * (r10(2) - r20(2))) * (r20(1) - r10(1));
+odot_denominator = (r10(2) - r20(2))^2 + (r20(1) - r10(1))^2;
+omegadot_Bot = odot_numerator/odot_denominator;
+
+
+
+
+
+
 
 
 
