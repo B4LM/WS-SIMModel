aac8c47c042ea3ff3ad771fd4fc017174297cfad
B4LM <salziger.grund_0s@icloud.com>
2025-02-10 12:38:50 +0100

    10022025_Vormittag

--- Kinematics_test.m
+++ Kinematics_test.m
@@ -1,26 +1,27 @@
-omega1 = 10;
+omega1 = 2 ;
 omega2 = 1;
 omegadot1 = 1;
 omegadot2 = 1;
-phi = 5;
-%phi = phideg * 180/pi;
+phi = 30;
+rs = [0;0];
 
-phivec = [cosd(phi); sind(phi)];
+RotPhi = [cosd(phi) -sind(phi); sind(phi) cosd(phi)];
 
-%r1x = (1/3)*cos(phi)* B_dis - (1/2)*sin(phi)*Achsabstand;
-%r1y = (1/3)*sin(phi)* B_dis + (1/2)*cos(phi)*Achsabstand;
-r1 = [(1/3)*phivec(1)* B_dis - (1/2)*phivec(2)*Achsabstand;(1/3)*phivec(2)* B_dis + (1/2)*phivec(1)*Achsabstand];
-%r2x = (1/3)*cos(phi)* B_dis + (1/2)*sin(phi)*Achsabstand;
-%r2y = (1/3)*sin(phi)* B_dis - (1/2)*cos(phi)*Achsabstand;
-r2 = [(1/3)*phivec(1)* B_dis + (1/2)*phivec(2)*Achsabstand;(1/3)*phivec(2)* B_dis - (1/2)*phivec(1)*Achsabstand];
+r10 = rs + RotPhi * [B_dis/3 ; Achsabstand/2];
+r20 = rs + RotPhi * [B_dis/3 ; -Achsabstand/2];
 
-v1 = phivec * omega1 * Reifen_Radius;
-v2 = phivec * omega2 * Reifen_Radius;
+v10 = RotPhi * [1;0] * omega1 * Reifen_Radius;
+v20 = RotPhi * [1;0] * omega2 * Reifen_Radius;
 
-Avel = [1 0 r1(1) ; 0 1 r1(2); 1 0 r2(1); 0 1 r2(2)];
-Vel_Result = linsolve(Avel,[v1;v2])
+Avel = [1 0 r10(1) ; 0 1 r10(2); 1 0 r20(1); 0 1 r20(2)];
+Vel_Result = linsolve(Avel,[v10;v20])
 
-a1 = phivec * omegadot1 * Reifen_Radius;
-a2 = phivec * omegadot2 * Reifen_Radius;
+%a1 = phivec * omegadot1 * Reifen_Radius;
+%a2 = phivec * omegadot2 * Reifen_Radius;
+r_neu = sqrt(B_dis^2 +(Achsabstand/2)^2);
+v1_neu = omega1 * Reifen_Radius;
+v2_neu = omega2 * Reifen_Radius;
+Vel_Result_neu = linsolve([1 r_neu; 1 r_neu], [v1_neu; v2_neu])
+
 
 
