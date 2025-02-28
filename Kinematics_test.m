66839f6014ca70cef492cb56b189d91f4e9116fa
B4LM <158221249+B4LM@users.noreply.github.com>
2025-02-28 18:35:16 +0100

    28022025

--- Kinematics_test.m
+++ Kinematics_test.m
@@ -1,19 +1,20 @@
-omega1 = 2 ;
-omega2 = 7;
-omegadot1 = 3;
-omegadot2 = 1;
-phi = 30;
+omega1 = 90 ;
+omega2 = 1550;
+omegadot1 = 0;
+omegadot2 = 0;
+phi = 0;
 rs = [0;0];
-omegabotdot = 2;
+omega_GB1 = Motoruebersetzung * omega1;
+omega_GB2 = Motoruebersetzung * omega2;
 
 RotPhi = [cosd(phi) -sind(phi); sind(phi) cosd(phi)];
 
-r10 = rs + RotPhi * [B_dis/3 ; Achsabstand/2];
-r20 = rs + RotPhi * [B_dis/3 ; -Achsabstand/2];
+r10 = rs + RotPhi * [-B_dis*Xi; Achsabstand/2];
+r20 = rs + RotPhi * [-B_dis*Xi ; -Achsabstand/2];
 
-v10 = RotPhi * [1;0] * omega1 * Reifen_Radius;
-v20 = RotPhi * [1;0] * omega2 * Reifen_Radius;
-vs = RotPhi * [1;0] * ((omega1 * Reifen_Radius + omega2 * Reifen_Radius)/2);
+v10 = RotPhi * [1;0] * omega_GB1 * Reifen_Radius;
+v20 = RotPhi * [1;0] * omega_GB2 * Reifen_Radius;
+vs = RotPhi * [1;0] * ((omega_GB1 * Reifen_Radius + omega_GB2 * Reifen_Radius)/2);
 
 o_numerator = (v20(1) - v10(1)) * (r10(2) - r20(2)) + (v20(2) - v10(2)) * (r20(1) - r10(1));
 o_denominator = (r10(2) - r20(2))^2 + (r20(1) - r10(1))^2;
@@ -22,31 +23,11 @@ omega_Bot = o_numerator / o_denominator;
 a10 = RotPhi * [1;0] * omegadot1 * Reifen_Radius;
 a20 = RotPhi * [1;0] * omegadot2 * Reifen_Radius;
 as = RotPhi * [1;0] * ((omegadot1 * Reifen_Radius + omegadot2 * Reifen_Radius)/2);
+as_bot = RotPhi' * as; 
 
 odot_numerator = (a20(1) - a10(1) - omega_Bot^2 * (r10(1) - r20(1))) * (r10(2) - r20(2)) + (a20(2) - a10(2) - omega_Bot^2 * (r10(2) - r20(2))) * (r20(1) - r10(1));
 odot_denominator = (r10(2) - r20(2))^2 + (r20(1) - r10(1))^2;
 omegadot_Bot = odot_numerator/odot_denominator;
-
-%Formeln in Notizen übersichtlicher!
-
-%Berechnung-Kraftrichtung-Block-Reibkräfte-> Alpha
-%Geschwindigkeitspol xp
-xp = (Achsabstand/2) * ((omega1 *Reifen_Radius + omega2 * Reifen_Radius)/(omega2 * Reifen_Radius-omega1 *Reifen_Radius));
-%Berechnung-Winkel
-alpha = atan(B_dis / xp);
-
-%Brechnung-Boden-Kontakt-Kräfte
-
-Fg_Bot = m_ges * g;                 %Schwerpunkt bekannt
-%F_Reifen = Fg_Bot * ((1-Xi)/2);
-F_Block = Fg_Bot * (Xi/2);
-
-%Brechung der Lastmomente T1 & T2
-
-T1 = -(Reifen_Radius/2)* (m_ges * as(1) - (2/Achsabstand)* I_Bot * omegabotdot - F_Block * (2*cos(alpha) + (4/Achsabstand)*B_dis*sin(alpha)*(1-Xi)));
-
-T2 = -(Reifen_Radius/2)* (m_ges * as(1) + (2/Achsabstand)* I_Bot * omegabotdot - F_Block * (2*cos(alpha) - (4/Achsabstand)*B_dis*sin(alpha)*(1-Xi)));
-
 
 
 
--- Parameter.m
+++ Parameter.m
@@ -3,10 +3,10 @@ clc; clear; close all;
 %% general
 
 g = 9.81;                    % [m/s^2]
-m_ges = 0.53;                % [kg]
-m_last = 0.51;               % [kg]
+m_ges = 1;                   % [kg]
+m_last = 0.86;               % [kg]
 Achsabstand = 0.15;          % [m]    
-Motoruebersetzung = 1/50;    % [1]
+Motoruebersetzung = 1/100;   % [1]
 B_dis = 0.25;                % [m]
 R_dis = 0.15;                % [m]
 Xi = 2/3;                    % [1]
@@ -16,48 +16,48 @@ mue_g = 0.35;                % [1]
 
 %% Motormodell
 
-motorinertia = 0.847 * 10^-6;% [kg*m^2] 
-motorinductivity = 0.0005;   % [H]
-motorresistance =  3;        % [Ohm]
-motordamping = 0.0001;        % [?] [0.005-> alt]
-motortorquekoef = 0.002;   % [N*m / A] [0.06533-> alt] [0.00174]
-motorBackEMFkoef = 0.002;  % [?] [0.06533-> alt]
+motorinertia = 0.847 * 10^-6;  % [kg*m^2] 
+motorinductivity = 0.0005;     % [H]
+motorresistance =  3;          % [Ohm]
+motordamping = 4.8 * 10^-6;    % [N*m*s] [0.005-> alt]
+motortorquekoef = 0.0038;      % [N*m / A] [0.06533-> alt] [0.00174]
+motorBackEMFkoef = 0.0038;     % [V*s / rad] [0.06533-> alt]
 
 %% Reifenmodell
 
-m_Reifen = 0.01;             % [kg]
+m_Reifen = 0.038;             % [kg]
 mue = 0.002;                 % [1]
-Reifen_Radius = 0.05;        % [m]
+Reifen_Radius = 0.04;        % [m]
 
 %% motor state space model
 
-A_alt = [0 1 0; 0 -motordamping/motorinertia motortorquekoef/motorinertia;0 -motorBackEMFkoef/motorinductivity -motorresistance/motorinductivity];
-B = [0; 0; 1/motorinductivity];
-CT = [0 1 0];
-D = [0];
-
-x0 = [0; 0.01; 0];
+% A_alt = [0 1 0; 0 -motordamping/motorinertia motortorquekoef/motorinertia;0 -motorBackEMFkoef/motorinductivity -motorresistance/motorinductivity];
+% B = [0; 0; 1/motorinductivity];
+% CT = [0 1 0];
+% D = [0];
+% 
+% x0 = [0; 0; 0];
 
 %% second try state space model
 
-A22 = -motordamping/(motorinertia + (m_Reifen * Reifen_Radius^2)/50);
-A23 = motortorquekoef/(motorinertia + (m_Reifen * Reifen_Radius^2)/50);
-A32 = -motorBackEMFkoef/motorinductivity;
-A33 = -motorresistance/motorinductivity;
-
-A_neu = [0 1 0; 0 A22 A23; 0 A32 A33];
-B_neu = [0; 0; 1/motorinductivity];
-E_neu = [0; -((m_last/2 + m_Reifen) / (motorinertia + (m_Reifen *Reifen_Radius^2)/50) * g * mue * Reifen_Radius); 0 ];
-CT_omega = [0 1 0];
-CT_phi = [1 0 0];
-CT_i = [0 0 1];
-x0_neu = [0; 0.01; 0];
+% A22 = -motordamping/(motorinertia + (m_Reifen * Reifen_Radius^2)/50);
+% A23 = motortorquekoef/(motorinertia + (m_Reifen * Reifen_Radius^2)/50);
+% A32 = -motorBackEMFkoef/motorinductivity;
+% A33 = -motorresistance/motorinductivity;
+% 
+% A_neu = [0 1 0; 0 A22 A23; 0 A32 A33];
+% B_neu = [0; 0; 1/motorinductivity];
+% E_neu = [0; -((m_last/2 + m_Reifen) / (motorinertia + (m_Reifen *Reifen_Radius^2)/50) * g * mue * Reifen_Radius); 0 ];
+% CT_omega = [0 1 0];
+% CT_phi = [1 0 0];
+% CT_i = [0 0 1];
+% x0_neu = [0; 0.01; 0];
 
 %% third try state space model
-A22n = -motordamping/motorinertia;
-A23n = motortorquekoef/motorinertia;
-A_neu2 = [0 1 0; 0 A22n A23n; 0 A32 A33];
-E_neu2 = [0; 0; 0 ];
+% A22n = -motordamping/motorinertia;
+% A23n = motortorquekoef/motorinertia;
+% A_neu2 = [0 1 0; 0 A22n A23n; 0 A32 A33];
+% E_neu2 = [0; 0; 0 ];
 
 %% testing
 
@@ -65,10 +65,10 @@ E_neu2 = [0; 0; 0 ];
 
 %omega = (1/(motordamping +((motortorquekoef^2)/motorresistance))) * ((motortorquekoef/motorresistance) * TestV - (m_last/2 + m_Reifen) * g *mue * Reifen_Radius)
 
-AA = [-(motordamping/motorinertia) motortorquekoef/motorinertia -(1/motorinertia); -motorBackEMFkoef/motorinductivity -motorresistance/motorinductivity 0; 0 0 0 ];
-BB = [0;1/motorinductivity;0];
-CCT = [0 0 1];
-xx0 = [ 0; 0; 0];
+% AA = [-(motordamping/motorinertia) motortorquekoef/motorinertia -(1/motorinertia); -motorBackEMFkoef/motorinductivity -motorresistance/motorinductivity 0; 0 0 0 ];
+% BB = [0;1/motorinductivity;0];
+% CCT = [0 0 1];
+% xx0 = [ 0; 0; 0];
 
 %% Sym-try
 % input: V
@@ -156,10 +156,29 @@ x0 = [0; 0];
 [y, t, x] = lsim(sys, u, t, x0);
 
 % Ergebnis plotten
-plot(t, 0.6*(y/pi))
+plot(t, y* (0.6/pi)) 
 xlabel('Zeit (s)')
 ylabel('n_G [rpm]')
 title('Systemantwort auf Eingangsgröße')
 grid on
 
+%% ruhelage
+U_ein = 12;
+T_ein = 0;
+nm = (motorresistance / (motortorquekoef^2 +  motorresistance * motordamping)) * ((motortorquekoef / motorresistance) * U_ein - T_ein)% * (60/(2*pi))/50;
+% x1 = linspace(0,0.1,1000); 
+% nmt1 = (motorresistance ./ (x1.^2 +  motorresistance * motordamping)) .* ((x1 / motorresistance) * U_ein - T_ein) * (60/(2*pi))/50;
+% 
+% plot(x1, nmt1)
+% xlim([0 0.1])
 
+%%%%%
+
+% syms x1
+% f = (motorresistance / (x1^2 +  motorresistance * motordamping)) * ((x1 / motorresistance) * U_ein - T_ein) * (60/(2*pi))/100;
+% f2 = diff(f,x1)==0;
+% extreme_points = solve(f2,x1);
+% extreme_values = subs(f, x1, extreme_points);
+% [maxX, maxidx] = max(extreme_values);
+% best_location = extreme_points(maxidx);
+% best_value = simplify(maxX, 'steps', 50);
