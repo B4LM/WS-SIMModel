7fd1217d4439ac4f60b3bfef62d02a05ec810562
B4LM <158221249+B4LM@users.noreply.github.com>
2025-03-24 16:20:41 +0100

    24032025

--- Parameter.m
+++ Parameter.m
@@ -1,10 +1,15 @@
 clc; clear; close all; 
 
+%% Wegpunkte
+
+StartPos = [0.3;0.3];     
+Zielpunkt = [1;1.2];
+
 %% general
 
 g = 9.81;                    % [m/s^2]
 m_ges = 1;                   % [kg]
-m_last = 0.424;               % [kg]
+m_last = 0.424;              % [kg]
 Achsabstand = 0.15;          % [m]    
 Motoruebersetzung = 100;     % [1]
 B_dis = 0.25;                % [m]
@@ -13,110 +18,35 @@ Xi = 2/3;                    % [1]
 I_Bot = 0.0072;              % [kg*m^2]
 mue_g = 0.35;                % [1]
 
-startPos = [0.3;0.3];
-waypoint = [1;1.2];
-
 
 %% Motormodell
 
-% motorinertia = 0.847 * 10^-6;  % [kg*m^2] 
-% motorinductivity = 0.0005;     % [H]
-% motorresistance =  3;          % [Ohm]
-% %motordamping = 2.5* 10^-6;    % [N*m*s] [0.005-> alt] [1.2 * 10^-6;]
-% motordamping = 0;    % [N*m*s] [0.005-> alt] [1.2 * 10^-6;]
-% motortorquekoef = 0.065;      % [N*m / A] [0.06533-> alt] [0.00174] [0.0019]
-% motorBackEMFkoef = 0.065;     % [V*s / rad] [0.06533-> alt][0.0019]
-
-% motorinertia = 5 * 10^-8;       % [kg*m^2] 
-% motorinductivity = 100 *10^-6;  % [H]
-% motorresistance =  13.33;       % [Ohm]
-% motordamping = 7.216 * 10^-4;    % [N*m*s]
-% motortorquekoef = 0.302;      % [N*m / A]
-% motorBackEMFkoef = 0.302;     % [V*s / rad]
-% V_brush = 1.5;                  % [V]
-
-motorinertia = 0.847 * 10^-6;        % [kg*m^2] 
-motorinductivity = 0.0002;       % [H]
-motorresistance =  13.33;            % [Ohm]
-motordamping = 9.12 * 10^-8;          % [N*m*s][7.216 * 10^-4];
-motortorquekoef = 0.0174;      % [N*m / A]
-motorBackEMFkoef = 0.0035;     % [V*s / rad]
+motor_traegheit = 0.847 * 10^-6;    % [kg*m^2] 
+motor_Induktivitaet = 0.0002;       % [H]
+motor_Widerstand =  13.33;          % [Ohm]
+motor_Daempfung = 9.12 * 10^-8;     % [N*m*s][7.216 * 10^-4];
+motor_Drehmomentkoef = 0.0174;      % [N*m / A]
+motor_BackEMFkoef = 0.0035;         % [V*s / rad]
 
 %% Reifenmodell
 
-m_Reifen = 0.038;             % [kg]
-mue = 0.002;                 % [1]
-Reifen_Radius = 0.04;        % [m]
+m_Reifen = 0.038;                % [kg]
+mue = 0.002;                     % [1]
+Reifen_Radius = 0.04;            % [m]
 
-%% motor state space model
-
-% A_alt = [0 1 0; 0 -motordamping/motorinertia motortorquekoef/motorinertia;0 -motorBackEMFkoef/motorinductivity -motorresistance/motorinductivity];
-% B = [0; 0; 1/motorinductivity];
-% CT = [0 1 0];
-% D = [0];
-% 
-% x0 = [0; 0; 0];
-
-%% second try state space model
-
-% A22 = -motordamping/(motorinertia + (m_Reifen * Reifen_Radius^2)/50);
-% A23 = motortorquekoef/(motorinertia + (m_Reifen * Reifen_Radius^2)/50);
-% A32 = -motorBackEMFkoef/motorinductivity;
-% A33 = -motorresistance/motorinductivity;
-% 
-% A_neu = [0 1 0; 0 A22 A23; 0 A32 A33];
-% B_neu = [0; 0; 1/motorinductivity];
-% E_neu = [0; -((m_last/2 + m_Reifen) / (motorinertia + (m_Reifen *Reifen_Radius^2)/50) * g * mue * Reifen_Radius); 0 ];
-% CT_omega = [0 1 0];
-% CT_phi = [1 0 0];
-% CT_i = [0 0 1];
-% x0_neu = [0; 0.01; 0];
-
-%% third try state space model
-% A22n = -motordamping/motorinertia;
-% A23n = motortorquekoef/motorinertia;
-% A_neu2 = [0 1 0; 0 A22n A23n; 0 A32 A33];
-% E_neu2 = [0; 0; 0 ];
-
-%% testing
-
-%TestV = 12;
-
-%omega = (1/(motordamping +((motortorquekoef^2)/motorresistance))) * ((motortorquekoef/motorresistance) * TestV - (m_last/2 + m_Reifen) * g *mue * Reifen_Radius)
-
-% AA = [-(motordamping/motorinertia) motortorquekoef/motorinertia -(1/motorinertia); -motorBackEMFkoef/motorinductivity -motorresistance/motorinductivity 0; 0 0 0 ];
-% BB = [0;1/motorinductivity;0];
-% CCT = [0 0 1];
-% xx0 = [ 0; 0; 0];
-
-%% Sym-try
-% input: V
-% output: i -> T
-
-%% Operating point
-
-x0s = [
-    0;
-    0;
-    ];
-
-u0 = [0 ; 0];
-z0 = 0;
-
-%% Symbolic variables
-
+%% symbolische Variablen
 
 x_sym = sym('x',[2 1]);
 u_sym = sym('u',[2 1]);
 z_sym = sym('z',[1 1]);
 y_sym = sym('y',[1 1]);
 
-%% Symbolic functions
+%% symbolische Funktionen
 
 % Momentengleichgewicht im Motor (Last als Eingangsgröße)
-f1_sym = -(motorresistance/motorinductivity) * x_sym(1) - (motorBackEMFkoef/motorinductivity) * x_sym(2)  + (1/motorinductivity)* u_sym(1);
+f1_sym = -(motor_Widerstand/motor_Induktivitaet) * x_sym(1) - (motor_BackEMFkoef/motor_Induktivitaet) * x_sym(2)  + (1/motor_Induktivitaet)* u_sym(1);
 % Maschenregel in Motor
-f2_sym = (motortorquekoef/motorinertia) * x_sym(1)- (motordamping/motorinertia) * x_sym(2)  - (1/motorinertia)*u_sym(2);
+f2_sym = (motor_Drehmomentkoef/motor_traegheit) * x_sym(1)- (motor_Daempfung/motor_traegheit) * x_sym(2)  - (1/motor_traegheit)*u_sym(2);
 
 
 %% state functions
@@ -129,6 +59,19 @@ f_sym = [
 g_sym = [
     x_sym(2);
     ];
+
+%% initial values
+x0s = [
+    0;
+    0;
+    ];
+
+u0 = [
+    0;
+    0;
+    ];
+
+z0 = 0;
 
 %% system matrices (symbolic)
 
@@ -159,68 +102,17 @@ d = 0;
 
 sys = ss(A,b,c,d);
 
-
-%% Sys-Test
-
-% Zeitvektor
-t = 0:0.01:10; 
-
-% Eingangsgröße (z.B. sinusförmig)
-u = ones(length(t), 1) * [12, 0]; %3.466
-
-% Anfangszustand (optional)
-x0 = [0; 0];
-
-% Simulation
-[y, t, x] = lsim(sys, u, t, x0);
-
-% Ergebnis plotten
-plot(t, y* (0.6/pi)) 
-xlabel('Zeit (s)')
-ylabel('n_G [rpm]')
-title('Systemantwort auf Eingangsgröße')
-grid on
-
-%% ruhelage
-U_ein = 12;
-T_ein = 0;
-nm = (motorresistance / (motortorquekoef*motorBackEMFkoef +  motorresistance * motordamping)) * ((motortorquekoef / motorresistance) * U_ein - T_ein) * (60/(2*pi*Motoruebersetzung))
-x1 = linspace(0,1,1000); 
-nmt1 = (motorresistance ./ (x1.^2 +  motorresistance * motordamping)) .* ((x1 / motorresistance) * U_ein - T_ein) * (60/(2*pi))* (1/Motoruebersetzung);
-%plot(x1, nmt1)
-xlim([0 1])
-
-syms tl
-ftl = (motorresistance / (motortorquekoef*motorBackEMFkoef +  motorresistance * motordamping)) * ((motortorquekoef / motorresistance) * U_ein - tl) * (60/(2*pi*Motoruebersetzung));
-fplot(ftl,[0 0.02])
-
-%%%%
-
-syms x1
-f = (motorresistance / (x1^2 +  motorresistance * motordamping)) * ((x1 / motorresistance) * U_ein - T_ein) * (60/(2*pi))/100;
-f2 = diff(f,x1)==0;
-extreme_points = solve(f2,x1);
-extreme_values = subs(f, x1, extreme_points);
-[maxX, maxidx] = max(extreme_values);
-best_location = extreme_points(maxidx);
-best_value = simplify(maxX, 'steps', 50);
-
 %% Map
 
 myMap = binaryOccupancyMap(1.8,1.8,100);
 
 walls = zeros(180,180);
-walls(15,15:165) = 1; % Top wall
-walls(165,15:165) = 1; % Bottom wall
-walls(15:165,15) = 1; % Left wall
-walls(15:165,165) = 1; % Right wall
-walls((180-waypoint(2)*100)-1:(180-waypoint(2)*100)+1,(waypoint(1)*100)-1:(waypoint(1)*100)+1) = 1; %waypoint
+walls(15,15:165) = 1; % Nord-Wand
+walls(165,15:165) = 1; % Süd-Wand
+walls(15:165,15) = 1; % West-Wand
+walls(15:165,165) = 1; % Ost-Wand
+walls((180-Zielpunkt(2)*100)-1:(180-Zielpunkt(2)*100)+1,(Zielpunkt(1)*100)-1:(Zielpunkt(1)*100)+1) = 1; %Zielpunkt
 
 
 setOccupancy(myMap,[1 1], walls, "grid")
-%show(myMap)
-
-% %% Test
-% U_ein = 12;
-% oms = (motortorquekoef/(motorresistance*motordamping+motortorquekoef*motorBackEMFkoef))*(U_ein-V_brush)
-% oms_rpm = oms * (60*Motoruebersetzung)/(2*pi)
+show(myMap)
