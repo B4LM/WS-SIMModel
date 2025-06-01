1e94dfa2d3346abd79ed86c2d004f17dcfa63006
B4LM <158221249+B4LM@users.noreply.github.com>
2025-06-01 18:29:43 +0200

    01062025

--- Model1Sensibilities.m
+++ Model1Sensibilities.m
@@ -1,5 +1,5 @@
 clc; clear; close all; 
-% Modell 2 Sensibilitäten-> ohne Schleifblöcke
+% Modell 1 Sensibilitäten-> mit Schleifblöcke!
 
 %% Sim Parameters
 t_span = [0 1.5];            % [s]
@@ -41,17 +41,21 @@ p_struct.motor_Widerstand = motor_Widerstand;
 p_struct.motor_Induktivitaet = motor_Induktivitaet;
 p_struct.motor_BackEMFkoef = motor_BackEMFkoef;
 p_struct.I_Reifen = I_Reifen;
+p_struct.mue_g = mue_g;
+p_struct.Xi = Xi;
+p_struct.B_dis = B_dis;
+p_struct.g = g;
 
 %% Syms
 
 syms i_m1 om_m1 i_m2 om_m2 xpos ypos theta real
 syms Ue1 Ue2 real
-syms p_Motoruebersetzung p_motor_traegheit p_m_ges p_Reifen_Radius p_I_Bot p_Achsabstand p_motor_Drehmomentkoef p_motor_Daempfung p_motor_Widerstand p_motor_Induktivitaet p_motor_BackEMFkoef p_I_Reifen real
+syms p_Motoruebersetzung p_motor_traegheit p_m_ges p_Reifen_Radius p_I_Bot p_Achsabstand p_motor_Drehmomentkoef p_motor_Daempfung p_motor_Widerstand p_motor_Induktivitaet p_motor_BackEMFkoef p_I_Reifen p_mue_g p_Xi p_B_dis p_g real
 
 x_sym = [i_m1; om_m1;i_m2; om_m2; xpos; ypos; theta];
 u_sym = [Ue1; Ue2];
-params_sym = [p_Motoruebersetzung; p_motor_traegheit; p_m_ges; p_Reifen_Radius; p_I_Bot; p_Achsabstand; p_motor_Drehmomentkoef; p_motor_Daempfung; p_motor_Widerstand; p_motor_Induktivitaet; p_motor_BackEMFkoef; p_I_Reifen];
-params_sym_ordered = {p_Motoruebersetzung, p_motor_traegheit, p_m_ges, p_Reifen_Radius, p_I_Bot, p_Achsabstand, p_motor_Drehmomentkoef, p_motor_Daempfung, p_motor_Widerstand, p_motor_Induktivitaet, p_motor_BackEMFkoef, p_I_Reifen};
+params_sym = [p_Motoruebersetzung; p_motor_traegheit; p_m_ges; p_Reifen_Radius; p_I_Bot; p_Achsabstand; p_motor_Drehmomentkoef; p_motor_Daempfung; p_motor_Widerstand; p_motor_Induktivitaet; p_motor_BackEMFkoef; p_I_Reifen; p_mue_g; p_Xi; p_B_dis; p_g];
+params_sym_ordered = {p_Motoruebersetzung, p_motor_traegheit, p_m_ges, p_Reifen_Radius, p_I_Bot, p_Achsabstand, p_motor_Drehmomentkoef, p_motor_Daempfung, p_motor_Widerstand, p_motor_Induktivitaet, p_motor_BackEMFkoef, p_I_Reifen,p_mue_g, p_Xi, p_B_dis, p_g};
 
 %% xdot-symbolic
 J_Mn_sym = p_Motoruebersetzung^2 * p_motor_traegheit;
@@ -64,10 +68,41 @@ M21_sym = M12_sym;
 M22_sym = M11_sym;
 
 J_g_sym = [M11_sym M12_sym;
-       M21_sym M22_sym];
+           M21_sym M22_sym];
 
-M_belastungen_sym = [p_Motoruebersetzung * p_motor_Drehmomentkoef * i_m1 - p_Motoruebersetzung^2 * p_motor_Daempfung*om_m1;
-                 p_Motoruebersetzung * p_motor_Drehmomentkoef * i_m2 - p_Motoruebersetzung^2 * p_motor_Daempfung*om_m2];
+v_Bot_sym = (p_Reifen_Radius/2)*(om_m1+om_m2);
+k_go = 1000; %steilheit für Übergang von 0 nach 1-> Fkt muss glatt sein
+v_tresh_sym = 1e-3;
+go_sym = 0.5 * (1 + tanh(k_go * (v_Bot_sym - v_tresh_sym)));  % Übergang für Reibkraft, muss glatt sein, funktioniert nur mit v>0!!!!!!
+
+%Glätten der Funktion
+delta_om_sym = om_m2-om_m1;
+sum_om_sym = om_m2+ om_m1;
+eps_om_sym = 1e-9;
+k_sum_om = 1000;
+sum_om_safe_sym = sum_om_sym + tanh(k_sum_om *sum_om_sym) * eps_om_sym +eps_om_sym;
+base_frac_sym = (2 * p_B_dis /p_Achsabstand) * delta_om_sym / sum_om_safe_sym;
+
+k_smooth_frac = 1000;
+smooth_factor_straight_sym = 0.5 * (1 - tanh(k_smooth_frac * (delta_om_sym^2-sym(eps_om_sym^2))));
+
+frac_sym = base_frac_sym * (1-smooth_factor_straight_sym) + (p_B_dis / 1e6) * smooth_factor_straight_sym;
+
+% if abs(om_m1 - om_m2) < eps_om
+%     xp_sym = 1e6;  % quasi geradeaus
+% else
+%     xp_sym = (p_Achsabstand/2)*((om_m1 + om_m2)/(om_m2 - om_m1));
+% end
+% 
+% frac_sym = p_B_dis/xp_sym;
+Frb_sym = sign(v_Bot_sym)*go_sym*p_mue_g *p_m_ges * p_g * (p_Xi/2);
+calpha_sym = frac_sym * sqrt(1+frac_sym^2)/(1+frac_sym^2);
+salpha_sym = sqrt(1+frac_sym^2)/(1+frac_sym^2);
+Tr1_sym = - p_Reifen_Radius * Frb_sym * salpha_sym-((2*p_Reifen_Radius)/p_Achsabstand)*Frb_sym*calpha_sym*(1-p_Xi)*p_B_dis;
+Tr2_sym = - p_Reifen_Radius * Frb_sym * salpha_sym+((2*p_Reifen_Radius)/p_Achsabstand)*Frb_sym*calpha_sym*(1-p_Xi)*p_B_dis;
+
+M_belastungen_sym = [p_Motoruebersetzung * p_motor_Drehmomentkoef * i_m1 - p_Motoruebersetzung^2 * p_motor_Daempfung*om_m1+ Tr1_sym;
+                     p_Motoruebersetzung * p_motor_Drehmomentkoef * i_m2 - p_Motoruebersetzung^2 * p_motor_Daempfung*om_m2+ Tr2_sym];
 
 om_dot_sym = inv(J_g_sym) * M_belastungen_sym;
 
@@ -98,7 +133,7 @@ x0 = [0;
        StartPos(2);
        0;];
 
-S0 = zeros(7,12);
+S0 = zeros(7,16);
 X_aug_0 = [x0;S0(:)];
 
 %% ODE-Sim
@@ -110,9 +145,9 @@ x_traj = X_aug_sol(:,1:7);
 S_sol = X_aug_sol(:,8:end);
 
 num_t_steps = length(T);
-S_traj_tensor = zeros(num_t_steps,7,12);
+S_traj_tensor = zeros(num_t_steps,7,16);
 for i = 1:num_t_steps
-    S_traj_tensor(i,:,:) = reshape(S_sol(i,:), 7, 12);
+    S_traj_tensor(i,:,:) = reshape(S_sol(i,:), 7, 16);
 end
 
 %% plot-solutions - relative Sensitivities
@@ -123,87 +158,12 @@ for k = 1:length(p_nom_values)
 end
 s_list = {'i_m1','om_m1','i_m2','om_m2','xpos','ypos','theta'};
 s_list_latex = {'$i_{M1}$','$\omega_{M1}$','$i_{M2}$','$\omega_{M2}$','$x_{pos}$','$y_{pos}$','$\theta$'};
-p_list_latex = {'Motoruebersetzung', '$Traegheit_M$', '$m_{ges}$', '$R_{Reifen}$', '$I_{Bot}$', 'Achsabstand', '$Drehmomentkoef_M$', '$Daempfung_M$', '$\Omega_M$', '$Induktivitaet_M$', '$BackEMFkoef_M$', '$I_{Reifen}$'};
+p_list_latex = {'Motoruebersetzung', '$Traegheit_M$', '$m_{ges}$', '$R_{Reifen}$', '$I_{Bot}$', 'Achsabstand', '$Drehmomentkoef_M$', '$Daempfung_M$', '$\Omega_M$', '$Induktivitaet_M$', '$BackEMFkoef_M$', '$I_{Reifen}$', '$\mu_g$', '$Xi$', '$B_{dis}$', 'g'};
 
 S_rel_tensor = zeros(size(S_traj_tensor));
 for kp = 1:length(p_list)
     S_rel_tensor(:,:,kp) = S_traj_tensor(:,:,kp) * p_nom_values(kp);
 end
-
-%plot every relative Sensibilities alone
-% again = true;
-% counter = 0;
-% while again == true
-%     while true
-%         if counter ~=0
-%             stop_input = input([newline 'another plot? (Y / N)'], 's');
-%             if strcmpi(stop_input, "Y")
-%                 close all;
-%                 break;
-%             elseif strcmpi(stop_input, "N")
-%                 again = false;
-%                 break;
-%             else
-%                 disp('input not valid');
-%             end
-%         else
-%             break
-%         end
-%     end
-% 
-%     if ~again
-%         break;
-%     end
-% 
-%     while true
-%         state_input = input([newline 'chose interested state:' newline 'i_m1-> 1' newline 'om_m1-> 2' newline 'i_m2-> 3' newline 'om_m2-> 4' newline 'xpos-> 5' newline 'ypos-> 6' newline 'theta-> 7' newline 'stop plotting-> End' newline 'input:'], 's');
-%         if strcmpi(state_input, "End")
-%             again = false;
-%             break;
-%         end
-%         state_val = str2double(state_input);
-%         if ~isnan(state_val) && mod(state_val, 1) == 0 && state_val >= 1 && state_val <= 7
-%             state_idx = state_val;
-%             break;
-%         else
-%             disp('input not valid');
-%         end
-%     end
-% 
-%     if ~again
-%         break;
-%     end
-% 
-%     while true
-%         param_input = input([newline 'chose interested parameter:' newline 'Motoruebersetzung-> 1' newline 'motor_traegheit-> 2' newline 'm_ges-> 3' newline 'Reifen_Radius-> 4' newline 'I_Bot-> 5' newline 'Achsabstand-> 6' newline 'motor_Drehmomentkoef-> 7' newline 'motor_Daempfung-> 8' newline 'motor_Widerstand-> 9' newline 'motor_Induktivitaet-> 10' newline 'motor_BackEMFkoef-> 11' newline 'I_Reifen-> 12' newline 'stop plotting-> End' newline 'input:'], 's');
-%         if strcmpi(param_input, "End")
-%             again = false;
-%             break;
-%         end
-% 
-%         param_val = str2double(param_input);
-%         if ~isnan(param_val) && mod(param_val, 1) == 0 && param_val>= 1 && param_val <= 12
-%             param_idx = param_val;
-%             break;
-%         else
-%             disp('input not valid');
-%         end
-%     end
-% 
-%     if ~again
-%         break;
-%     end
-% 
-%     counter = counter+1;
-%     figure;
-%     plot(T,S_rel_tensor(:,state_idx,param_idx))
-%     title(['relative sensitvity (p_k*dS/dp_k) of', s_list{state_idx},'to',p_list{param_idx}]);
-%     xlabel('time in [s]');
-%     ylabel('(p_k*dS/dp_k)')
-%     grid on;
-% end
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % plot the sensibilities of a state to every parameter
 again = true;
@@ -264,10 +224,9 @@ end
 
 
 
-
 %% Diff Function
 
-function xdot = Dynamics(x,u,Motoruebersetzung, motor_traegheit, m_ges, Reifen_Radius, I_Bot, Achsabstand, motor_Drehmomentkoef, motor_Daempfung,motor_Widerstand, motor_Induktivitaet, motor_BackEMFkoef,I_Reifen)
+function xdot = Dynamics(x,u,Motoruebersetzung, motor_traegheit, m_ges, Reifen_Radius, I_Bot, Achsabstand, motor_Drehmomentkoef, motor_Daempfung,motor_Widerstand, motor_Induktivitaet, motor_BackEMFkoef,I_Reifen,mue_g,  Xi, B_dis,g)
 i_m1 = x(1);
 om_m1 = x(2);
 i_m2 = x(3);
@@ -289,10 +248,51 @@ M22 = M11;
 J_g = [M11 M12;
        M21 M22];
 
-M_belastungen = [Motoruebersetzung * motor_Drehmomentkoef * i_m1 - Motoruebersetzung^2 * motor_Daempfung*om_m1;
-                 Motoruebersetzung * motor_Drehmomentkoef * i_m2 - Motoruebersetzung^2 * motor_Daempfung*om_m2];
+v_Bot = (Reifen_Radius/2)*(om_m1+om_m2);
+k_go = 1000; %steilheit für Übergang von 0 nach 1-> Fkt muss glatt sein
+v_tresh = 1e-3;
+go= 0.5 * (1 + tanh(k_go * (v_Bot - v_tresh)));  % Übergang für Reibkraft, muss glatt sein, funktioniert nur mit v>0!!!!!!
 
-om_dot = inv(J_g) * M_belastungen;
+%Glätten der Funktion
+delta_om = om_m2-om_m1;
+sum_om = om_m2+ om_m1;
+eps_om = 1e-9;
+k_sum_om = 1000;
+sum_om_safe = sum_om + tanh(k_sum_om * sum_om) * eps_om +eps_om;
+base_frac = (2 * B_dis /Achsabstand) * delta_om / sum_om_safe;
+
+k_smooth_frac = 1000;
+smooth_factor_straight = 0.5 * (1 - tanh(k_smooth_frac * (delta_om^2-eps_om^2)));
+
+frac = base_frac * (1-smooth_factor_straight) + (B_dis / 1e6) * smooth_factor_straight;
+
+
+% v_Bot = (Reifen_Radius/2)*(om_m1+om_m2);
+% if v_Bot < 1e-3
+%     go = 0;  % quasi stillstehend
+% else
+%     go = 1;
+% end
+% 
+% eps_om = 1e-6;
+% if abs(om_m1 - om_m2) < eps_om
+%     xp = 1e6;  % quasi geradeaus
+% else
+%     xp = (Achsabstand/2)*((om_m1 + om_m2)/(om_m2 - om_m1));
+% end
+% 
+% frac = B_dis/xp;
+
+Frb = sign(v_Bot)*go*mue_g *m_ges * g * (Xi/2);
+calpha = frac * sqrt(1+frac^2)/(1+frac^2);
+salpha = sqrt(1+frac^2)/(1+frac^2);
+Tr1 = - Reifen_Radius * Frb * salpha-((2*Reifen_Radius)/Achsabstand)*Frb*calpha*(1-Xi)*B_dis;
+Tr2 = - Reifen_Radius * Frb * salpha+((2*Reifen_Radius)/Achsabstand)*Frb*calpha*(1-Xi)*B_dis;
+
+M_belastungen = [Motoruebersetzung * motor_Drehmomentkoef * i_m1 - Motoruebersetzung^2 * motor_Daempfung*om_m1 + Tr1;
+                 Motoruebersetzung * motor_Drehmomentkoef * i_m2 - Motoruebersetzung^2 * motor_Daempfung*om_m2 + Tr2];
+
+om_dot = J_g \ M_belastungen;
 
 di1dt = -(motor_Widerstand/motor_Induktivitaet) * i_m1 - (Motoruebersetzung * motor_BackEMFkoef/motor_Induktivitaet) * om_m1 + (1/motor_Induktivitaet)* Ue1;
 dom1dt = om_dot(1);
@@ -309,14 +309,14 @@ end
 
 function dX_aug_dt = AugmentedDynamics(t, X_aug, u, p_struct, A_fun, B_fun)
 x = X_aug(1:7);
-S_matrix = reshape(X_aug(8:end),7,12);
+S_matrix = reshape(X_aug(8:end),7,16);
 
-x_dot = Dynamics(x,u,p_struct.Motoruebersetzung, p_struct.motor_traegheit, p_struct.m_ges, p_struct.Reifen_Radius, p_struct.I_Bot, p_struct.Achsabstand, p_struct.motor_Drehmomentkoef, p_struct.motor_Daempfung,p_struct.motor_Widerstand, p_struct.motor_Induktivitaet, p_struct.motor_BackEMFkoef,p_struct.I_Reifen);
-A_jacobian = A_fun(x,u,p_struct.Motoruebersetzung, p_struct.motor_traegheit, p_struct.m_ges, p_struct.Reifen_Radius, p_struct.I_Bot, p_struct.Achsabstand, p_struct.motor_Drehmomentkoef, p_struct.motor_Daempfung, p_struct.motor_Widerstand, p_struct.motor_Induktivitaet, p_struct.motor_BackEMFkoef, p_struct.I_Reifen);
-B_jacobian = B_fun(x,u,p_struct.Motoruebersetzung, p_struct.motor_traegheit, p_struct.m_ges, p_struct.Reifen_Radius, p_struct.I_Bot, p_struct.Achsabstand, p_struct.motor_Drehmomentkoef, p_struct.motor_Daempfung, p_struct.motor_Widerstand, p_struct.motor_Induktivitaet, p_struct.motor_BackEMFkoef, p_struct.I_Reifen);
+x_dot = Dynamics(x,u,p_struct.Motoruebersetzung, p_struct.motor_traegheit, p_struct.m_ges, p_struct.Reifen_Radius, p_struct.I_Bot, p_struct.Achsabstand, p_struct.motor_Drehmomentkoef, p_struct.motor_Daempfung,p_struct.motor_Widerstand, p_struct.motor_Induktivitaet, p_struct.motor_BackEMFkoef,p_struct.I_Reifen, p_struct.mue_g, p_struct.Xi, p_struct.B_dis, p_struct.g);
+A_jacobian = A_fun(x,u,p_struct.Motoruebersetzung, p_struct.motor_traegheit, p_struct.m_ges, p_struct.Reifen_Radius, p_struct.I_Bot, p_struct.Achsabstand, p_struct.motor_Drehmomentkoef, p_struct.motor_Daempfung, p_struct.motor_Widerstand, p_struct.motor_Induktivitaet, p_struct.motor_BackEMFkoef, p_struct.I_Reifen, p_struct.mue_g, p_struct.Xi, p_struct.B_dis,p_struct.g);
+B_jacobian = B_fun(x,u,p_struct.Motoruebersetzung, p_struct.motor_traegheit, p_struct.m_ges, p_struct.Reifen_Radius, p_struct.I_Bot, p_struct.Achsabstand, p_struct.motor_Drehmomentkoef, p_struct.motor_Daempfung, p_struct.motor_Widerstand, p_struct.motor_Induktivitaet, p_struct.motor_BackEMFkoef, p_struct.I_Reifen, p_struct.mue_g, p_struct.Xi, p_struct.B_dis, p_struct.g);
 
-S_dot_matrix = zeros(7,12);
-for j = 1:12
+S_dot_matrix = zeros(7,16);
+for j = 1:16
     S_j = S_matrix(:,j);
     df_dpj = B_jacobian(:,j);
     S_dot_matrix(:,j) = A_jacobian * S_j + df_dpj;
